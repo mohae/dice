f13675d2d41bc881ecdc9fd7f4d52947c28a3842
Joel Scoble <joel.scoble@outlook.com>
2015-10-13 18:57:26 +0000

    add example app

---
+++ roll/roll.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+  "fmt"
+  "flag"
+  "os"
+  "strconv"
+
+  "github.com/mohae/dice"
+)
+
+var (
+  sides = flag.Int("sides", 6, "number of sides for the die")
+)
+
+
+// Usage is a replacement usage function for the flags package.
+func Usage() {
+    fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+    fmt.Fprintf(os.Stderr, "  rolldice [-sides] n\n")
+    fmt.Fprintf(os.Stderr, "\twhere n is the number of times you want to roll the die\n")
+    fmt.Fprintf(os.Stderr, "Flags:\n")
+    flag.PrintDefaults()
+    os.Exit(2)
+}
+
+func main() {
+  flag.Usage = Usage
+  flag.Parse()
+  args := flag.Args()
+  var rolls int
+  var err error
+  if len(args) > 0 {
+    rolls, err = strconv.Atoi(args[0])
+    if err != nil {
+      fmt.Fprintf(os.Stderr, "Could not convert %s to an int: %s", args[0], err)
+      os.Exit(1)
+    }
+  }
+  // rolls must be > 0
+  if rolls== 0 {
+    rolls = 1
+  }
+  d := dice.New(*sides)
+  for i := 0; i < rolls; i++ {
+    v, err := d.Roll()
+    if err != nil {
+      fmt.Fprintln(os.Stderr, err)
+      os.Exit(1)
+    }
+    fmt.Println(v)
+  }
+  os.Exit(0)
+}
